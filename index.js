b4589e24079cfbd5f6a258a8273cb744eb75f4e1
beastboioioi <beastboioioi.dev@gmail.com>
2026-01-10 12:11:22 +0000

    Add --premake-version command and many fixes

--- index.js
+++ index.js
@@ -1,28 +1,76 @@
-const core      = require( '@actions/core' );
-const github    = require( '@actions/github' );
-const toolcache = require( '@actions/tool-cache' );
+const core      = require('@actions/core');
+const toolcache = require('@actions/tool-cache');
+const fs        = require('fs');
+const path      = require('path');
 
-async function downloadPremake() {
-	if ( process.platform === 'win32' ) {
-		const fileZip = await toolcache.downloadTool( `https://github.com/premake/premake-core/releases/download/v5.0.0-beta4/premake-5.0.0-beta4-windows.zip` );
-		await toolcache.extractZip( fileZip, 'tpremake' );
+function GetPremakeVersion() 
+{
+	const arg = process.argv.find(a => a.startsWith('--premake-version='));
+	return arg ? arg.split('=')[1] : '5.0.0-beta4';
+}
+
+const PREMAKE_VERSION = GetPremakeVersion();
+const BASE_URL = `https://github.com/premake/premake-core/releases/download/v${PREMAKE_VERSION}`;
+
+async function DownloadPremake() 
+{
+	let archiveName;
+	let extractFunction;
+	
+	switch( process.platform )
+	{
+		case 'win32':
+		{
+			archiveName = `premake-${PREMAKE_VERSION}-windows.zip`;
+			extractFunction = toolcache.extractZip;
+		} break;
+
+		case 'darwin':
+		{
+			archiveName = `premake-${PREMAKE_VERSION}-macosx.tar.gz`;
+			extractFunction = toolcache.extractTar;
+		} break;
+
+		default:
+		{
+			archiveName = `premake-${PREMAKE_VERSION}-linux.tar.gz`;
+			extractFunction = toolcache.extractTar;
+		} break;
 	}
-	else if ( process.platform === 'darwin' ) {
-		const fileZip = await toolcache.downloadTool( `https://github.com/premake/premake-core/releases/download/v5.0.0-beta4/premake-5.0.0-beta4-macosx.tar.gz` );
-		await toolcache.extractTar( fileZip, 'tpremake' );
+
+	const url = `${BASE_URL}/${archiveName}`;
+	core.info(`Downloading Premake ${PREMAKE_VERSION} from ${url}`);
+
+	const archivePath = await toolcache.downloadTool(url);
+	const extractedPath = await extractFunction( archivePath );
+
+	return extractedPath;
+}
+
+function MakeExecutableIfNeeded( extractedPath ) 
+{
+	if( process.platform === 'win32' ) return;
+
+	const premakeBin = path.join( extractedPath, 'premake5' );
+
+	core.info( `Making Premake executable: ${premakeBin}` );
+	fs.chmodSync( premakeBin, 0o755 );
+}
+
+async function main()
+{
+	try 
+	{
+		const premakeDir = await DownloadPremake();
+		MakeExecutableIfNeeded( premakeDir );
+
+		core.addPath( premakeDir );
+		core.info( `Premake added to PATH: ${premakeDir}` );
 	}
-	else {
-		const fileZip = await toolcache.downloadTool( `https://github.com/premake/premake-core/releases/download/v5.0.0-beta4/premake-5.0.0-beta4-linux.tar.gz` );
-		await toolcache.extractTar( fileZip, 'tpremake' );
+	catch( error ) 
+	{
+		core.setFailed( `Premake setup failed: ${error.message || error}` );
 	}
 }
 
-async function main() {
-	core.addPath( "tpremake" );
-
-	downloadPremake();
-}
-
-main().catch( err => {
-	core.setFailed( `Error when downloading premake error: ${err}` )
-})
+main();
